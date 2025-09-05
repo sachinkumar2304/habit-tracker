9f174b95db2598f4d30843c914fa70eb0717e819
Sachin Kumar <spal67073@gmail.com>
2025-09-05 14:49:15 +0530

    Fix allowed hosts for render

--- habit_project/settings.py
+++ habit_project/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_v4fbor+fq=gu%m3o@5b1=a)i_h)@sp0aw2435&rqa4#9zq!k^'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['habitracker-h4dy.onrender.com',
+    '127.0.0.1',
+    'localhost'
+    ]
 
 
 # Application definition
@@ -42,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -111,6 +115,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+
+# Agar media files use kar rahe ho to yeh add karo
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
